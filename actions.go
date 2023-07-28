5a56a9c55c54b769bb8a73c8b86c24654e937583
henderjon <list@box370.net>
2023-07-27 23:58:31 -0500

    changes word to clarify comment

--- actions.go
+++ actions.go
@@ -27,7 +27,7 @@ func doAppend(b buffer, l1 int) error {
 	return b.insertAfter(l1, false)
 }
 
-// doDelete moves a range of lines to the end of the buffer then moves the last line up to "forget" about the lines at the end
+// doDelete moves a range of lines to the end of the buffer then decreases the last line to "forget" about the lines at the end
 func doDelete(b buffer, l1, l2 int) error {
 	if l1 <= 0 {
 		return errors.New("doDelete; invalid address")
