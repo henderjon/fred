362ec1ef7953ad2375ffd2bebb9b3a0e6c2dd6b6
henderjon <list@box370.net>
2023-08-07 12:32:05 -0500

    introduces search{}; a struct for storing previous search info

---
+++ search.go
@@ -0,0 +1,10 @@
+package main
+
+type search struct {
+	reverse bool // this is NOT forward because forward is the default, but the zero value of a bool false; "forward" should be used in surrounding code
+	pattern string
+}
+
+func (s search) isZero() bool {
+	return len(s.pattern) == 0
+}
