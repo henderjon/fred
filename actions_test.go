7bb08c4aeea3d79cab28db51baf4a314deafd02f
henderjon <list@box370.net>
2023-08-10 16:27:01 -0500

    fixes glob test

--- actions_test.go
+++ actions_test.go
@@ -373,6 +373,7 @@ func Test_doGlob(t *testing.T) {
 	for i, test := range tests {
 		input := getInput(os.Stdin, os.Stdout)
 		controlBuffer := getTestActionBuffer()
+		doGlobMarks(test.cmd, controlBuffer)
 		doGlob(test.cmd, controlBuffer, input, &cache{})
 
 		if diff := cmp.Diff(controlBuffer, test.expected, cmp.AllowUnexported(memoryBuf{}, bufferLine{}, search{})); diff != "" {
